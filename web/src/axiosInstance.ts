e4bbfb917145c8a651b9b3cf92ecae85977d7e9c
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-08-08 17:20:46 +0800

    web: update axiosInstance base url

--- web/src/axiosInstance.ts
+++ web/src/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-	baseURL:  import.meta.env.VITE_BASE_URL || 'http://localhost:3333/api/',
+	baseURL: 'https://registry-api-qpfw.onrender.com/api/',
 	timeout: 10000,
 	headers: {
 		'Content-Type': 'application/json'
