6944e4c86a7af1e4053df6da72aa32fae5702541
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-08-18 08:39:25 +0800

    server: update program service and controller

--- server/src/main/kotlin/com/ralphmarondev/registry/controller/ProgramController.kt
+++ server/src/main/kotlin/com/ralphmarondev/registry/controller/ProgramController.kt
@@ -3,27 +3,46 @@ package com.ralphmarondev.registry.controller
 import com.ralphmarondev.registry.dto.ProgramRequest
 import com.ralphmarondev.registry.dto.ProgramResponse
 import com.ralphmarondev.registry.service.ProgramService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("program/")
 class ProgramController(
-    private val beneficiaryProgramService: ProgramService
+    private val programService: ProgramService
 ) {
     @PostMapping
     fun create(@RequestBody request: ProgramRequest): ResponseEntity<ProgramResponse> {
-        val beneficiaryProgram = beneficiaryProgramService.create(request)
-        return ResponseEntity.status(201).body(beneficiaryProgram)
+        val program = programService.create(request)
+        return ResponseEntity.status(HttpStatus.CREATED).body(program)
     }
 
     @GetMapping
     fun getAll(): List<ProgramResponse> {
-        return beneficiaryProgramService.getAll()
+        return programService.getAll()
+    }
+
+    @GetMapping("{id}/")
+    fun getById(id: Long): ResponseEntity<ProgramResponse> {
+        return ResponseEntity.ok().body(programService.getById(id))
+    }
+
+    @PutMapping("{id}/")
+    fun update(
+        @PathVariable id: Long,
+        @RequestBody request: ProgramRequest
+    ): ResponseEntity<ProgramResponse> {
+        return ResponseEntity.ok(programService.update(id, request))
+    }
+
+    @DeleteMapping("{id}/")
+    fun delete(@PathVariable id: Long): ResponseEntity<Any> {
+        return ResponseEntity.ok(programService.delete(id))
     }
 
     @PostMapping("batch/")
     fun batch(@RequestBody requests: List<ProgramRequest>): ResponseEntity<List<ProgramResponse>> {
-        return ResponseEntity.status(201).body(beneficiaryProgramService.batch(requests))
+        return ResponseEntity.status(HttpStatus.CREATED).body(programService.batch(requests))
     }
 }
--- server/src/main/kotlin/com/ralphmarondev/registry/service/ProgramService.kt
+++ server/src/main/kotlin/com/ralphmarondev/registry/service/ProgramService.kt
@@ -4,31 +4,72 @@ import com.ralphmarondev.registry.dto.ProgramRequest
 import com.ralphmarondev.registry.dto.ProgramResponse
 import com.ralphmarondev.registry.entity.Program
 import com.ralphmarondev.registry.exception.ResourceAlreadyExistsException
+import com.ralphmarondev.registry.exception.ResourceNotFoundException
 import com.ralphmarondev.registry.mapper.toResponse
 import com.ralphmarondev.registry.repository.ProgramRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 @Service
 @Transactional
 class ProgramService(
-    private val beneficiaryRepository: ProgramRepository
+    private val programRepository: ProgramRepository
 ) {
     fun getAll(): List<ProgramResponse> {
-        return beneficiaryRepository.findAll()
+        return programRepository.findAll()
             .filter { !it.isDeleted }
             .map { it.toResponse() }
     }
 
+    fun getById(id: Long): ProgramResponse? {
+        return programRepository.findById(id)
+            .filter { !it.isDeleted }
+            .orElseThrow { ResourceNotFoundException("Program not found.") }
+            .toResponse()
+    }
+
     fun create(request: ProgramRequest): ProgramResponse {
-        if (beneficiaryRepository.findByName(request.name) != null) {
-            throw ResourceAlreadyExistsException("Beneficiary name already exists")
+        val existingProgram = programRepository.findByName(request.name)
+
+        if (existingProgram != null && !existingProgram.isDeleted) {
+            throw ResourceAlreadyExistsException("Program already exists")
         }
         val program = Program(
             name = request.name,
             description = request.description
         )
-        return beneficiaryRepository.save(program).toResponse()
+        return programRepository.save(program).toResponse()
+    }
+
+    fun update(id: Long, request: ProgramRequest): ProgramResponse {
+        val program = programRepository.findById(id)
+            .orElseThrow { ResourceNotFoundException("Program not found.") }
+        val existingProgram = programRepository.findByName(request.name)
+
+        if (program.name != request.name &&
+            existingProgram != null &&
+            !existingProgram.isDeleted
+        ) {
+            throw ResourceAlreadyExistsException("Program '${request.name}' already exists.")
+        }
+        val updatedProgram = program.copy(
+            name = request.name,
+            description = request.description,
+            updateDate = LocalDateTime.now()
+        )
+        return programRepository.save(updatedProgram).toResponse()
+    }
+
+    fun delete(id: Long): ProgramResponse {
+        val program = programRepository.findById(id)
+            .filter { !it.isDeleted }
+            .orElseThrow { ResourceNotFoundException("Program not found.") }
+        val deletedProgram = program.copy(
+            isDeleted = true,
+            updateDate = LocalDateTime.now()
+        )
+        return programRepository.save(deletedProgram).toResponse()
     }
 
     fun batch(requests: List<ProgramRequest>): List<ProgramResponse> {
